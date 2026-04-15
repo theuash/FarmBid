76fb0ca7044cac482ce735e8b913828a8aca0490
theuash <theuash@gmail.com>
2026-04-15 18:53:44 +0530

    okie

--- backend/config/razorpay.js
+++
@@ -1,9 +0,0 @@
-const Razorpay = require('razorpay');
-
-// Ensure Razorpay gets initialized correctly
-const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID || 'rzp_test_YourKeyIdHere',
-  key_secret: process.env.RAZORPAY_KEY_SECRET || 'YourKeySecretHere',
-});
-
-module.exports = razorpay;
